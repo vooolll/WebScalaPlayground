335098e4e97ee706096adb28d4c2f88c1116701a
valeryi.baibossynov <valeryi.baibossynov@itrium.ru>
2016-12-22 15:44:07 +0300

    Deleted redudant braces

--- test/bootstrap/Goodies.scala
+++ test/bootstrap/Goodies.scala
@@ -3,6 +3,4 @@ package bootstrap
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{MustMatchers, OptionValues}
 
-trait Goodies extends MustMatchers with OptionValues with MockitoSugar {
-
-}
+trait Goodies extends MustMatchers with OptionValues with MockitoSugar
